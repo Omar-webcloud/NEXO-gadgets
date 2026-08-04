ae5146d0f37b547b0cf3b67898a14254bc04d43d
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-08-04 13:16:48 +0600

    fix: update Google site verification meta tag in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -136,7 +136,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="rFFlsMVXMOTWg6H0_zw9h7xy0TP_4pK0Hmwixt9Gzzo" />
+        <meta name="google-site-verification" content="AProJEuc056711v17-vX-ITQusdcNFOchItLKFqPNY0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
